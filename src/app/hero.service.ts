efe8764fc03aa7678fa3aec4a212a38b2d7d55b9
Jason Xiong <JasonJianXiong@gmail.com>
2017-12-02 13:52:12 -0500

    Dec 2

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,11 +11,16 @@ export class HeroService {
 //  return HEROES;
 //}
 //    
-getHeroes(): Observable<Hero[]> {
-  // Todo: send the message _after_ fetching the heroes
-  this.messageService.add('HeroService: fetched heroes');
-  return of(HEROES);
-}  
+//getHeroes(): Observable<Hero[]> {
+//  // Todo: send the message _after_ fetching the heroes
+//  this.messageService.add('HeroService: fetched heroes');
+//  return of(HEROES);
+//}  
+    getHero(id: number): Observable<Hero> {
+  // Todo: send the message _after_ fetching the hero
+  this.messageService.add(`HeroService: fetched hero id=${id}`);
+  return of(HEROES.find(hero => hero.id === id));
+}
 //  constructor() { }
 constructor(private messageService: MessageService) { }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import {HeroService} from '../hero.service';
-import { Hero } from '../hero';
+//import { Component, OnInit } from '@angular/core';
+//import {HeroService} from '../hero.service';
+//import { Hero } from '../hero';
+//
+//
+//@Component({
+//  selector: 'app-heroes',
+//  templateUrl: './heroes.component.html',
+//  styleUrls: ['./heroes.component.css']
+//})
+//export class HeroesComponent implements OnInit {
+////  hero : Hero = {
+////    id : 1,
+////    name : 'Jian Xiong'
+////  };
+//
+//    heroes: Hero[];
+////    heroes = HEROES;
+////    selectedHero: Hero;
+//
+////onSelect(hero: Hero): void {
+////  this.selectedHero = hero;
+////}
+//
+////  getHeroes(): void {
+////  this.heroes = this.heroService.getHeroes();
+////}   //a function to retrieve the heroes from the service.
+////    
+//    
+//getHeroes(): void {
+//  this.heroService.getHeroes()
+//      .subscribe(heroes => this.heroes = heroes);
+//}
+//    
+//
+//    
+//  constructor(
+//    private heroService: HeroService
+//    ) { }
+//
+//  ngOnInit() {
+//  this.getHeroes();
+//          
+//}
+//    
+//
+//}
 
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,41 +57,17 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-//  hero : Hero = {
-//    id : 1,
-//    name : 'Jian Xiong'
-//  };
+  heroes: Hero[];
 
-    heroes: Hero[];
-//    heroes = HEROES;
-    selectedHero: Hero;
-
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
-
-//  getHeroes(): void {
-//  this.heroes = this.heroService.getHeroes();
-//}   //a function to retrieve the heroes from the service.
-//    
-    
-getHeroes(): void {
-  this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-}
-    
-
-    
-  constructor(
-    private heroService: HeroService
-    ) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-  this.getHeroes();
-          
-}
-    
+    this.getHeroes();
+  }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
-
 
